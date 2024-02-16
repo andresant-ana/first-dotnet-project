31bbcafd8e58284a69710ab00114b14c2602b8bf
andresant-ana <rm551575@fiap.com.br>
2024-02-16 11:53:37 -0300

    Adicionar arquivos de projeto.

---
+++ rm551575/Controllers/AlunosController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace rm551575.Controllers
+{
+    public class AlunosController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
